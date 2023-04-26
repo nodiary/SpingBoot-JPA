e45652f1aa2ac1de98794c90148440385ac3d771
HP_W10 <HP_W10@DESKTOP-GQ49AJC>
2023-04-26 17:00:46 +0900

    김영한의 실전 스프링부트와 JPA 1, 2

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jpabook.jpashop.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	
+	List<Member> findByName(String name);
+	//Member findById(Long id);
+	
+
+}
